18703ff529e96076d3cb3e1983a1bd4d106e9dc3
Coops0 <51346812+Coops0@users.noreply.github.com>
2024-05-03 22:36:04 -0400

    dont need headers

--- readme.md
+++ readme.md
@@ -7,6 +7,6 @@
 
 I bought an old 90's CRT and I want it to basically play videos on loop in the background.\
 This hosts a web server on the local network that lets you upload videos and play them with VLC on loop, an optionally on mute or with a visualizer.\
-You then, can hook this up to the old CRT through various converters and it you should be able to leave the computer alone.\
+You then, can hook this up to the old CRT through various converters and it you should be able to leave the computer alone.
 
-This is not secured in any way so don't expose it to the internet.\
+This is not secured in any way so don't expose it to the internet.
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@ mod web_manager;
 
 use std::path::{Path, PathBuf};
 
-use axum::{
-    extract::Request,
-    middleware::{from_fn, Next},
-    response::Response,
-};
 use tokio::net::TcpListener;
 use vlc_manager::launch_vlc_thread;
 use web_manager::manager_router;
@@ -20,9 +15,7 @@ async fn main() {
     let _ = tokio::fs::create_dir("uploads/").await;
 
     let video_sender = launch_vlc_thread();
-    let app = manager_router()
-        .with_state(video_sender)
-        .layer(from_fn(headers));
+    let app = manager_router().with_state(video_sender);
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -31,14 +24,4 @@ async fn main() {
 #[must_use]
 pub fn video_path(path: &str) -> PathBuf {
     Path::new("uploads/").join(path)
-}
-
-async fn headers(request: Request, next: Next) -> Response {
-    let mut response = next.run(request).await;
-
-    response
-        .headers_mut()
-        .insert("Strict-Transport-Security", "max-age=3600".parse().unwrap());
-
-    response
 }
